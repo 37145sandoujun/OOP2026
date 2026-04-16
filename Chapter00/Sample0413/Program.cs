541caa5c7ae5bf00564fed3333101a942df2857c
infosys <infosys@C4218>
2026-04-16 10:56:12 +0900

    プロジェクト ファイルを追加します。

---
+++ Chapter00/Sample0413/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Sample0413
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("表示回数:");
+            string imputNum = Console.ReadLine();//入力
+
+            int count = int.Parse(imputNum);//整数への変換
+            int i = 0;
+            while (count > i)
+            {
+                    Console.Write(i + 1 + ":");
+                    Console.WriteLine(":Hello");//画面出力
+                i++;
+            }
+
+        }
+    }
+}
---
+++ Chapter00/Sample0415/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Sample0415
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum=0;
+            int[] ints = new int[10];
+            //入力
+            for (int i = 0; i < ints.Length; i++)
+            {
+                Console.Write("array[" + i + "]=");
+                ints[i] = int.Parse(Console.ReadLine());
+            }
+            //出力
+            for (int i = 0; i < ints.Length; i++)
+            {
+                Console.Write("ints[" + i + "]=" );
+                astOut(ints[i]);
+                
+                Console.WriteLine();
+            }
+            //合計
+            //for (int i = 0; i < ints.Length; i++)
+            //{
+            //    ints[i] += sum;
+               
+           // }
+            Console.WriteLine(ints.Where(n=>n % 2==0).Sum());
+        }
+        static void astOut(int num)
+        {
+            for (int j = 0; j < num; j++)
+            {
+                Console.Write("*");
+            }
+        }
+}
+}
