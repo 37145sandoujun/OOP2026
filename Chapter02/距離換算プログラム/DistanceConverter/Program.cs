7657e693c467597b62d82807001be9772e742b84
潤 山藤 <oibc37145@ojs.ac.jp>
2026-04-20 11:43:01 +0900

    ｐ３０コード23まで入力

---
+++ Chapter02/距離換算プログラム/DistanceConverter/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace DistanceConverter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            if (args.Length >= 1 && args[0] == "-tom")
+            {
+                //フィートからメートルへの対応表を出力
+                for (int feet = 0; feet <= 10; feet++)
+                {
+                    double meter = FeetToMeter(feet);
+                    Console.WriteLine($"{feet}ft={meter:0.0000}m");
+
+                }
+            }
+            else 
+            {
+                for (int meter = 0; meter <= 10; meter++)
+                {
+                    double feet = MeterToFeet(meter);
+                    Console.WriteLine($"{meter}ft={feet:0.0000}m");
+                }
+
+            }
+        }
+        static double FeetToMeter(int feet)//フィートからメートルを求める
+        { 
+            //feet*0.3048
+            return feet * 0.3048;
+        }
+        //メートルからフィートを求める
+        static double MeterToFeet(int meter)
+        {
+            //feet*0.3048
+            return meter/ 0.3048;
+        }
+
+
+    }
+}
