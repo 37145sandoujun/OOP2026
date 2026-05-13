11ca2385a4655d2ee178badfce5f2a2fe782fb9d
潤 山藤 <oibc37145@ojs.ac.jp>
2026-05-13 10:34:28 +0900

    データ入力対応

---
+++ Chapter02/演習問題/Exercise01/Program.cs
@@ -0,0 +1,46 @@
+﻿
+namespace Exercise01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("曲名：");
+            string? title = Console.ReadLine();
+            var songs = new List<Song>();
+            while (true)
+            {
+                if (title == "end")
+                {
+                    Console.Write("おわり");
+                    break;
+                }
+                else
+                {
+                    Console.Write("アーティスト名：");
+                    string? srtistname = Console.ReadLine();
+
+                    Console.Write("演奏時間（秒）：");
+                    int length = int.Parse(Console.ReadLine());
+                    Song song = new Song(title, srtistname, length);
+                    songs.Add(song);
+                    PrintSongs(songs);
+
+                }
+            }
+           
+        }
+        
+
+        public static void PrintSongs(IEnumerable<Song> songs)
+        {
+            foreach (var Song in songs)
+            {
+                //  new PrintSongs(@"Title\ArttistName\Length");
+                int mm = Song.Length / 60;
+                int ss = Song.Length % 60;
+                Console.WriteLine($"{Song.Title},{Song.ArtistName},{mm}:{ss:00}");
+            }
+        }
+    }
+}
