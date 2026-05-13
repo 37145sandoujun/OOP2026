e270ad14a1215be0943bd9b952fa1dfdc6f9ba94
潤 山藤 <oibc37145@ojs.ac.jp>
2026-05-13 11:20:14 +0900

    未完成

--- Chapter02/演習問題/Exercise02/Program.cs
+++ Chapter02/演習問題/Exercise02/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            PrintInchMeterList(1, 10);
+        }
+         private static void PrintInchMeterList(int start, int end){
+
+        }
+
+        private static void PrintMeterToFeetList(int start, int stop)
+        {
+            FeetConverter comverter = new FeetConverter();
+            for (int meter = start; meter <= stop; meter++)
+            {
+                double feet = FeetConverter.ToFeet(meter);
+                Console.WriteLine($"{meter}m={feet:0.0000}ft");
+            }
+
         }
     }
 }
