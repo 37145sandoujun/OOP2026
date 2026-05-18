62c04e84b7620d6f60a6ebb5e5173e6ec82de58a
潤 山藤 <oibc37145@ojs.ac.jp>
2026-05-18 11:43:58 +0900

    ラムダ式とはｐ６８

--- Chapter02/演習問題/Exercise02/Program.cs
+++ Chapter02/演習問題/Exercise02/Program.cs
@@ -14,9 +14,9 @@
        
         
             
-            for (int feet = start; feet <= stop; feet++)
+            for (int feet = start; feet <= end; feet++)
             {
-                double meter = InchConverter.ToMeter(feet);
+                double meter = InchConverter.FromMeter(feet);
                 Console.WriteLine($"{feet}inch={meter:0.0000}m");
             }
 
