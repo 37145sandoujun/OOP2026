a5ddab1bbb25f404c002d4e4c4565044a517013e
潤 山藤 <oibc37145@ojs.ac.jp>
2026-05-21 11:21:37 +0900

    問題3,2,(1,2,3,4)完成

---
+++ Chapter03/演習問題/Exercise02/Program.cs
@@ -0,0 +1,61 @@
+﻿
+namespace Exercise02 {
+    internal class Program {
+        static void Main(string[] args) {
+            var cities = new List<string> {
+                "Tokyo", "New Delhi", "Bangkok", "London",
+                "Paris", "Berlin", "Canberra", "Hong Kong",
+            };
+
+            Console.WriteLine("***** 3.2.1 *****");
+            Exercise2_1(cities);
+            Console.WriteLine();
+
+            Console.WriteLine("***** 3.2.2 *****");
+            Exercise2_2(cities);
+            Console.WriteLine();
+
+            Console.WriteLine("***** 3.2.3 *****");
+            Exercise2_3(cities);
+            Console.WriteLine();
+
+            Console.WriteLine("***** 3.2.4 *****");
+            Exercise2_4(cities);
+            Console.WriteLine();
+        }
+
+        private static void Exercise2_1(List<string> cities) {
+            //できたらGitのコメント「問題3.2.1完成」
+            var name = Console.ReadLine();
+            var exists = cities.FindIndex(s => s == name);
+            Console.WriteLine(exists);
+
+        }
+
+        private static void Exercise2_2(List<string> cities) {
+            //できたらGitのコメント「問題3.2.2完成」
+            var exists = cities.Count(s => s.Contains('o'));
+
+            Console.WriteLine(exists);
+        }
+
+        private static void Exercise2_3(List<string> cities) {
+            //できたらGitのコメント「問題3.2.3完成」
+            var query = cities.Where(s => s.Contains('o')).ToList();
+            foreach (var item in query)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        private static void Exercise2_4(List<string> cities) {
+            //できたらGitのコメント「問題3.2.4完成」
+            var select = cities.Where(s => s.Contains('B')).ToList();
+            
+                foreach(var item in select)
+            {
+                Console.WriteLine(item.Length);
+            }
+        }
+    }
+}
