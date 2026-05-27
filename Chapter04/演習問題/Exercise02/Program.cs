6b8274ea8c673ef13a37ec5283995dc734e40af8
潤 山藤 <oibc37145@ojs.ac.jp>
2026-05-27 12:06:11 +0900

    問題4，2開始

---
+++ Chapter04/演習問題/Exercise02/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Exercise02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-----4.2.1----");
+        }
+    }
+}
