f87622ecab9764e5ef71e8704ec6061159e1cd28
潤 山藤 <oibc37145@ojs.ac.jp>
2026-06-11 10:33:28 +0900

    問題６，３，５まで

---
+++ Chapter06/文字列の比較/Section01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+
+namespace Section01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var str1 = "Windows";
+            var str2 = "WINDOWS";
+
+            var cultureInfo = new CultureInfo("Ja-ap");
+            if (String.Compare(str1, str2, cultureInfo,
+                CompareOptions.IgnoreWidth|CompareOptions.IgnoreCase) ==0){
+                Console.WriteLine("等しい");
+            }
+            else
+            {
+                Console.WriteLine("等しくない");
+            }
+        }
+    }
+}
---
+++ Chapter06/演習問題/Exercise03/Program.cs
@@ -0,0 +1,67 @@
+﻿
+namespace Exercise03 {
+    internal class Program {
+        static void Main(string[] args) {
+            var text = "Jackdaws love my big sphinx of quartz";
+            #region
+            Console.WriteLine("6.3.1");
+            Exercise1(text);
+
+            Console.WriteLine("6.3.2");
+            Exercise2(text);
+
+            Console.WriteLine("6.3.3");
+            Exercise3(text);
+
+            Console.WriteLine("6.3.4");
+            Exercise4(text);
+
+            Console.WriteLine("6.3.5");
+            Exercise5(text);
+
+            Console.WriteLine("6.3.99");
+            Exercise6(text);
+            #endregion
+        }
+
+        private static void Exercise1(string text) {
+            var spaces = text.Count(c => c == ' ');
+            Console.WriteLine($"空白数:{spaces}");
+            //別の書き方
+            //Console.WriteLine("空白数:{0}", spaces);
+        }
+
+        private static void Exercise2(string text) {
+            Console.Write("検索：");
+            var search = Console.ReadLine();
+            Console.Write("置換：");
+            var replace = Console.ReadLine();
+
+            var replaced = text.Replace(search, replace);
+            Console.WriteLine(replaced);
+        }
+
+        private static void Exercise3(string text) {
+            //やらなくてよい
+        }
+
+        private static void Exercise4(string text) {
+            var count = text.Split( ' ').Length;
+            Console.WriteLine($"単語数:{count+1}");
+        }
+
+        private static void Exercise5(string text) {
+            //テスト出る
+             text.Split(' ').Where(s => s.Length <= 4).ToList().ForEach(Console.WriteLine); 
+            
+           
+        }
+
+        //アルファベットの数をカウントして表示する
+        private static void Exercise6(string text) {
+            //辞書を使った集計
+            var str = text.ToLower
+
+        }
+    }
+}
