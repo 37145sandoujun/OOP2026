0e87ddf4f30d5e64a7e2c981b10a05bed4d13e03
潤 山藤 <oibc37145@ojs.ac.jp>
2026-06-19 11:00:52 +0900

    演習問題７，１完成

--- Chapter07/演習問題/Exercise01/Program.cs
+++ Chapter07/演習問題/Exercise01/Program.cs
@@ -6,7 +6,7 @@ namespace Exercise01
         static void Main(string[] args)
         {
             int[] numbers = [5, 10, 17, 9, 3, 21, 10, 40, 21, 3, 35];
-            
+
 
             Console.WriteLine("7.1.1");
             Exercise1(numbers);
@@ -26,31 +26,49 @@ namespace Exercise01
 
         private static void Exercise1(int[] numbers)
         {
-            var max = numbers.Max(x => x);
-            Console.WriteLine("最大値"+max);
-
-            
-            
+            var max = numbers.Max();
+            Console.WriteLine("最大値" + max);
         }
 
         private static void Exercise2(int[] numbers)
         {
-            var length
+            var length = numbers.TakeLast(2);
+            foreach (var item in length)
+                Console.WriteLine(item);
+            {
+
+            }
         }
+
 
         private static void Exercise3(int[] numbers)
         {
-            throw new NotImplementedException();
+            var str = numbers.Select(x => x.ToString("000"));
+            foreach (var item in str)
+            {
+                Console.WriteLine(item);
+            }
+            
+
+            
         }
 
         private static void Exercise4(int[] numbers)
         {
-            throw new NotImplementedException();
+            var result = numbers.OrderBy(X=>X).Take(3);
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
         }
 
         private static void Exercise5(int[] numbers)
         {
-            throw new NotImplementedException();
+            var dis = numbers.Distinct().Count(X => X > 10);
+            Console.WriteLine(dis);
+
+            
+
         }
     }
 }
