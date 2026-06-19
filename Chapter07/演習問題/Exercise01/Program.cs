d4784fbd01f78b8decab055638872e7f05c24720
潤 山藤 <oibc37145@ojs.ac.jp>
2026-06-19 10:16:27 +0900

    問題１完成

--- Chapter07/演習問題/Exercise01/Program.cs
+++ Chapter07/演習問題/Exercise01/Program.cs
@@ -6,25 +6,39 @@ namespace Exercise01
         static void Main(string[] args)
         {
             int[] numbers = [5, 10, 17, 9, 3, 21, 10, 40, 21, 3, 35];
+            
 
-
-            Console.WriteLine("\n7.1.1");
+            Console.WriteLine("7.1.1");
             Exercise1(numbers);
 
-            Console.WriteLine("\n7.1.2");
+            Console.WriteLine("7.1.2");
             Exercise2(numbers);
 
-            Console.WriteLine("\n7.1.3");
+            Console.WriteLine("7.1.3");
             Exercise3(numbers);
 
-            Console.WriteLine("\n7.1.4");
+            Console.WriteLine("7.1.4");
             Exercise4(numbers);
 
-            Console.WriteLine("\n7.1.5");
+            Console.WriteLine("7.1.5");
             Exercise5(numbers);
         }
 
-        private static void Exercise5(int[] numbers)
+        private static void Exercise1(int[] numbers)
+        {
+            var max = numbers.Max(x => x);
+            Console.WriteLine("最大値"+max);
+
+            
+            
+        }
+
+        private static void Exercise2(int[] numbers)
+        {
+            var length
+        }
+
+        private static void Exercise3(int[] numbers)
         {
             throw new NotImplementedException();
         }
@@ -34,17 +48,7 @@ namespace Exercise01
             throw new NotImplementedException();
         }
 
-        private static void Exercise3(int[] numbers)
-        {
-            throw new NotImplementedException();
-        }
-
-        private static void Exercise2(int[] numbers)
-        {
-            throw new NotImplementedException();
-        }
-
-        private static void Exercise1(int[] numbers)
+        private static void Exercise5(int[] numbers)
         {
             throw new NotImplementedException();
         }
