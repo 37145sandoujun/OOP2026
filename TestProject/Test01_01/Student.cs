b7f1411772b108fdf760b160372bb42f9b52f74d
潤 山藤 <oibc37145@ojs.ac.jp>
2026-06-22 11:24:11 +0900

    設問１＿Studentクラス完成

--- TestProject/Test01_01/Student.cs
+++ TestProject/Test01_01/Student.cs
@@ -1,7 +1,9 @@
 ﻿namespace Test01_01 {
     public class Student {
-        
+        public string Name { get; set; } = string.Empty;
 
+        public string Subject { get; set; } = string.Empty;
 
+        public int Score { get; set; }
     }
 }
