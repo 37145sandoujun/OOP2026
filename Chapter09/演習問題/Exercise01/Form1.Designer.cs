bec08180593354c23496b8637cccd150bcb57b11
潤 山藤 <oibc37145@ojs.ac.jp>
2026-07-09 14:51:53 +0900

    演習問題完成

--- Chapter09/演習問題/Exercise01/Form1.Designer.cs
+++ Chapter09/演習問題/Exercise01/Form1.Designer.cs
@@ -70,6 +70,7 @@
             button1.TabIndex = 0;
             button1.Text = "①";
             button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
             // 
             // button2
             // 
@@ -80,6 +81,7 @@
             button2.TabIndex = 0;
             button2.Text = "②";
             button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
             // 
             // tbOut2
             // 
@@ -97,6 +99,7 @@
             button3.TabIndex = 0;
             button3.Text = "③";
             button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
             // 
             // tbOut3
             // 
--- Chapter09/演習問題/Exercise01/Form1.cs
+++ Chapter09/演習問題/Exercise01/Form1.cs
@@ -1,3 +1,6 @@
+using System.Globalization;
+using System.Text.RegularExpressions;
+
 namespace Exercise01
 {
     public partial class Form1 : Form
@@ -10,6 +13,32 @@ namespace Exercise01
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var now = DateTime.Now;
+
+            tbOut1.Text = now.ToString("yyyy/MM/dd HH:mm");
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var now =  DateTime.Now;
+            var culture = new CultureInfo("ja-JP");
+            culture.DateTimeFormat.Calendar = new JapaneseCalendar();
+            var str = now.ToString("ggyy年M月d年 HH時mm分ss秒", culture);
+            tbOut2.Text = $"{str}";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var now = DateTime.Now;
+            var culture = new CultureInfo("ja-JP");
+            culture.DateTimeFormat.Calendar = new JapaneseCalendar();
+            var str = now.ToString("gg yy年 M月 d年　(dddd)", culture);
+            tbOut3.Text = $"{str}";
         }
     }
 }
