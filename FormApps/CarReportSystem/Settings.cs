3e28be3de3fd2f7e26a5851c7a30c91166cdc10b
潤 山藤 <oibc37145@ojs.ac.jp>
2026-08-27 14:02:03 +0900

    SettingsクラスにLoad処理追加

--- FormApps/CarReportSystem/Settings.cs
+++ FormApps/CarReportSystem/Settings.cs
@@ -21,6 +21,20 @@ namespace CarReportSystem
         //外部からNEWさせない
         private Settings() { }
 
+        public void Load()
+        {
+            if (File.Exists(FileName))
+                return;
+
+            using var reader = XmlReader.Create(FileName);
+            var serializer = new XmlSerializer(typeof(SettingsData));
+
+            if(serializer.Deserialize(reader)is SettingsData data)
+            {
+                MainFormBackColor = data.MainForBackColor;
+            }
+        }
+
         public void Save()
         {
             var data = new SettingsData
